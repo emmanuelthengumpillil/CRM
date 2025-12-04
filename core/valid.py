ac8055d844e195ba07e6752554b0730f6711ede7
Emmanuel <emmanuelthengumpillil@gmail.com>
2025-12-04 07:53:50 +0530

    Added validate_key

--- core/valid.py
+++ core/valid.py
@@ -10,3 +10,16 @@ def valid_row_data(row_dict):
     else:
         return True, row_dict
 
+def validate_keys(file, row_dict):
+    count = 0
+    header = storage.read_csv(file)[2]
+    key_list = list(row_dict.keys())
+    for i in range(len(header)):
+        if header[i] == key_list[i]:
+            pass
+        else:
+            count += 1
+    if count != 0:
+        return False, None
+    else:
+        return True, row_dict
