cf82d4922f04d95b8ea9d94287dadc6803629a51
Emmanuel <emmanuelthengumpillil@gmail.com>
2025-12-04 22:13:22 +0530

    Added create_row_dict function

--- core/input.py
+++ core/input.py
@@ -4,14 +4,22 @@ def get_name():
     while True:
         name = input("Enter Name:- ")
         if name.isalpha():
-            return name, 200
+            return 200, name
+        print("Enter alphabet values")
 
 def get_phone():
     while True:
-        try:
-            phone = input("Enter Phone:- ")
-            if phone.isnumeric and 10 <= (len(phone)) <= 12:
-                return phone, 200
-        except ValueError:
-            return None, 404
+        phone = input("Enter Phone:- ")
+        if phone.isnumeric() and 10 <= (len(phone)) <= 12:
+            return 200, phone
+        return 400, None
 
+
+def create_row_dict(file):
+    keys = ["Name", "Phone"]
+    values = [get_name()[1], get_phone()[1]]
+    row_dict = dict(zip(keys, values))
+    if valid.validate_row(file, row_dict):
+        return 200, row_dict
+
+print(create_row_dict("crm.csv"))
