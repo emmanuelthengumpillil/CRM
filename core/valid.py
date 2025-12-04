5078169e8ae040d7530c289d8b9f1ac5a7229fb4
Emmanuel <emmanuelthengumpillil@gmail.com>
2025-12-04 19:27:07 +0530

    Added validate_not_empty and UPDATED validate_keys

--- core/valid.py
+++ core/valid.py
@@ -2,24 +2,27 @@ import storage
 
 
 row = {"Name":"qweqw","Phone":"13213"}
+row2 = {"Phone":"13213","Name":"qweqw"}
 f = "crm.csv"
+
 
 def valid_row_data(row_dict):
     if not isinstance(row_dict, dict):
         return False, None  
     else:
-        return True, row_dict
+        return True, row_dict, None
+
 
 def validate_keys(file, row_dict):
-    count = 0
     header = storage.read_csv(file)[2]
-    key_list = list(row_dict.keys())
-    for i in range(len(header)):
-        if header[i] == key_list[i]:
-            pass
-        else:
-            count += 1
-    if count != 0:
-        return False, None
-    else:
-        return True, row_dict
+    # set() will ignore it from checking the order
+    if not set(header) == set(row_dict.keys()):
+        return 405, False, None
+    return 200, True, row_dict, None
+
+
+def validate_not_empty(row_dict):
+    values = set(row_dict.values())
+    if values == "" or values is None:
+        return 404, False, None
+    return 200, True, None
