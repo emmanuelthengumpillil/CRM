921d6ef3ed4c4594a2cac379b3fd4522d56a18df
Emmanuel <emmanuelthengumpillil@gmail.com>
2025-12-06 17:37:02 +0530

    Added get_id and minor updates in get_name and get_phone

--- core/input.py
+++ core/input.py
@@ -4,15 +4,22 @@ def get_name():
     while True:
         name = input("Enter Name:- ")
         if valid.valid_name(name)[1]:
-            return 200, name
+            return 200, True, name
         print("Enter alphabet values")
 
 def get_phone():
     while True:
         phone = input("Enter Phone:- ")
         if valid.valid_phone(phone)[1]:
-            return 200, phone
-        
+            return 200, True, phone
+
+
+def get_id():
+    while True:
+        id = input("Enter ID:- ")
+        if id.isdigit():
+            return 200, True, id
+
 
 def create_row_dict(file):
     keys = ["Name", "Phone"]
@@ -21,4 +28,5 @@ def create_row_dict(file):
     if valid.validate_row(file, row_dict):
         return 200, row_dict
 
-# print(create_row_dict("crm.csv"))
+
+print(get_id())
