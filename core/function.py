953fba23c150dc25494b0075553dde77d4dd9f95
Emmanuel <emmanuelthengumpillil@gmail.com>
2025-12-07 09:30:44 +0530

    Added is_duplicate function

--- core/function.py
+++ core/function.py
@@ -6,6 +6,16 @@ f = "crm.csv"
 row1 = {"Name": "hello", "Phone" : "1234567890"}
 row2 = {"Phone" : "1234567890", "Name": "hello"}
 
+def is_duplicate(file):
+    name = input.get_name()[2]
+    phone = input.get_phone()[2]
+    crm = storage.read_csv(file)[1]
+    for row in crm:
+        if row["Name"] == name or row["Phone"] == phone:
+            return 200, True
+    return 404, False
+
+
 def create_row_dict(file):
     keys = ["ID", "Name", "Phone"]
     id = storage.get_next_id(file)
@@ -27,3 +37,20 @@ def add_person_crm(file):
     return 404, crm, False
 
 
+def remove_person_crm(file):
+    new_row = []
+    crm = storage.read_csv(file)[1]
+    old_row = create_row_dict(f)[1]
+    if valid.validate_row(file, old_row)[1] == True:
+        read_file = storage.read_csv(file)[1]
+        for row in read_file:
+            if row != old_row:
+                new_row.append(row)
+        # print(new_row)
+        storage.write_csv(file,new_row)
+        return 202, crm, None
+    return 404, crm, None 
+
+
+# print(remove_person_crm(f,row1))
+print(is_duplicate(f))
