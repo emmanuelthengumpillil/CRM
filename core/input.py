34e2d1639d7480b368f9939a2347b0f8fbf562f8
Emmanuel <emmanuelthengumpillil@gmail.com>
2025-12-09 21:44:20 +0530

    UPDATED inputs and ADDED get_input

--- core/input.py
+++ core/input.py
@@ -4,29 +4,25 @@ def get_name():
     while True:
         name = input("Enter Name:- ")
         if valid.valid_name(name)[1]:
-            return 200, True, name
+            return {"success":True, "data":name}
         print("Enter alphabet values")
 
 def get_phone():
     while True:
         phone = input("Enter Phone:- ")
         if valid.valid_phone(phone)[1]:
-            return 200, True, phone
+            return {"success":True, "data": phone}
 
 
 def get_id():
     while True:
         id = input("Enter ID:- ")
         if id.isdigit():
-            return 200, True, id
+            return {"success":True, "data": id}
 
 
-def create_row_dict(file):
-    keys = ["Name", "Phone"]
-    values = [get_name()[1], get_phone()[1]]
-    row_dict = dict(zip(keys, values))
-    if valid.validate_row(file, row_dict):
-        return 200, row_dict
-
-
-print(get_id())
+def get_input():
+    name = get_name()[2]
+    phone = get_phone()[2]
+    list = [name,phone]
+    return {"success":True, "data": list}
