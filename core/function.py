2ad7a631fcdc207019c7f4b6c92d797335c5a280
Emmanuel <emmanuelthengumpillil@gmail.com>
2025-12-29 14:31:55 +0530

    UPDATED return function and declared few functions

--- core/function.py
+++ core/function.py
@@ -11,13 +11,12 @@ def is_duplicate(file,row_list):
     result = storage.read_csv(file)
     if result["success"]:
         crm = result["data"]
-        for row in crm:        
-            name = row["Name"]
+        for row in crm:
             phone = row["Phone"]
             if row_list["Phone"] == phone:
-                return {"success":True, "msg":"duplicate of list is found"}
-        return {"success":False, "data":row_list}
-    return {"success":True, "msg":"Couldn't read csv"}
+                return {"success":False, "msg":"duplicate of list is found","location":"isduplicate"}
+        return {"success":True, "data":row_list, "location":"isduplicate"}
+    return {"success":False, "msg":"Couldn't read csv","location":"isduplicate"}
 
 
 def create_row_dict(file):
@@ -35,8 +34,8 @@ def create_row_dict(file):
                     values = [result_id["data"], name, phone]
                     row_dict = dict(zip(keys, values))
                 if valid.validate_row(file, row_dict)["success"]:
-                    return {"success":True, "data": row_dict}
-                return {"success":False, "error": "Created row_dict invalid"}
+                    return {"success":True, "data": row_dict, "location":"create_row_dict"}
+                return {"success":False, "error": "Created row_dict invalid", "location":"create_row_dict"}
             else:
                 n_id = storage.get_next_id(file)
                 if n_id != None:
@@ -44,10 +43,10 @@ def create_row_dict(file):
                         values = [n_id["data"], name, phone]
                         row_dict = dict(zip(keys, values))
                     if valid.validate_row(file, row_dict)["success"]:
-                        return {"success":True, "data": row_dict, "crm":crm}
-                    return {"success":False, "error": "Created row_dict invalid"}
-        return {"success":False, "error":"Csv/Header is empty"}
-    return {"success":False, "msg":"Couldn't read csv"}
+                        return {"success":True, "data": row_dict, "crm":crm, "location":"create_row_dict"}
+                    return {"success":False, "error": "Created row_dict invalid", "location":"create_row_dict"}
+        return {"success":False, "error":"Csv/Header is empty", "location":"create_row_dict"}
+    return {"success":False, "msg":"Couldn't read csv", "location":"create_row_dict"}
 
 
 def add_person_crm(file):
@@ -55,11 +54,11 @@ def add_person_crm(file):
     if row["success"]:
         data = row["data"]
         duplicate = is_duplicate(file,data)
-        print(duplicate)
-        if duplicate["success"] == False:
+        if duplicate["success"]:
             storage.write_csv(file, data)
-            return {"success":True, "msg":"Successfully added person"}
-    return {"success":False, "error":"Couldn't add person"}
+            return {"success":True, "msg":"Successfully added person", "location":"add_person"}
+        return {"success":True, "data":data, "location":"add_person"}
+    return {"success":False, "error":"Couldn't add person", "location":"add_person"}
 
 
 def remove_person_crm(file):
@@ -74,8 +73,8 @@ def remove_person_crm(file):
                     new_row.append(row)
             # print(new_row)
             storage.rewrite_csv(file,file,new_row)
-            return {"success":True, "data":new_row}
-    return {"success": False, "error": "File not found"}
+            return {"success":True, "data":new_row, "location":"remove_person"}
+    return {"success": False, "error": "File not found", "location":"remove_person"}
 
 
 def create_duplicate_file(old_file, new_file):
@@ -84,5 +83,15 @@ def create_duplicate_file(old_file, new_file):
         crm = result["data"]
         write_result = storage.rewrite_csv(old_file,new_file,crm)
         print(write_result)
+#create dupulicate not complete
 
-create_duplicate_file(f,"crmcopy.csv")
+def search_person_crm():
+    pass
+
+
+def view_crm():
+    pass
+
+
+def update_person_crm():
+    pass
