de61a4e99671c109a7e25dda6a3bf3e2e5634791
Emmanuel <emmanuelthengumpillil@gmail.com>
2026-01-03 08:28:01 +0530

    Added get_input()

--- core/inpt.py
+++ core/inpt.py
@@ -41,3 +41,5 @@ def get_choice():
 def get_file():
     while True:
         file = input("get file name:- ")
+        if valid.valid_name(file):
+            return {"success": True, "data": file}
