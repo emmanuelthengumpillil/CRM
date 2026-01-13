6715ae611fd5d181eacdf8266240ece674819635
Emmanuel <emmanuelthengumpillil@gmail.com>
2026-01-13 08:56:36 +0530

    Added working addperson and updated the logic in every condition

--- core/menu.py
+++ core/menu.py
@@ -6,52 +6,59 @@ import sys
 
 print("Welome to terminal crm")
 
-path_file = inpt.get_file()
-if path_file["success"]:
-    crm_file = storage.read_csv(path_file["data"])
-else:
-    print("Invlid File")
+# path_file = inpt.get_file()
+# if path_file["success"]:
+crm_file = storage.read_csv("data\\crm.csv")
+# else:
+    # print("Invlid File")
 
 while True:
     ch = inpt.get_choice()
-    if ch["success"] == False:
+    choice = ch["data"]
+    if ch["success"]:
+        if choice in ("add","a"):
+            name_res = inpt.get_name()
+            if not name_res["success"]:
+                print(name_res["error"])
+                sys.exit()
+            phone_res = inpt.get_phone()
+            if not phone_res["success"]:
+                print(phone_res["error"])
+                sys.exit()
+            add = customer.add_person_crm(crm_file, name_res["data"], phone_res["data"])
+            if add["success"]:
+                print("Success")
+            else:
+                print(add["error"])
+        elif choice in ("remove","r"):
+            remove = customer.remove_person_crm(crm_file)
+            if remove["success"]:
+                print("Success")
+            else:
+                print(remove["error"])
+        elif choice in ("search","s"):
+            srch = search.get_id_by_name(crm_file)
+            if srch["success"]:
+                print("Success")
+            else:
+                print(srch["error"])
+        elif choice in ("update","u"):
+            update = customer.update_person_crm(crm_file)
+            if update["success"]:
+                print("Success")
+            else:
+                print(update["error"])
+        elif choice in ("view","v"):
+            view = customer.view_crm(crm_file)
+            if view["success"]:
+                print("Success")
+            else:
+                print(view["error"])
+        elif choice in ("exit","e"):
+            sys.exit()
+        else:
+            print("Choice not recognised")
+    else:
         sys.exit()
 
-    if ch["data"] == "add":
-        name = inpt.get_name()["data"]
-        phone = inpt.get_phone()["data"]
-        add = customer.add_person_crm(crm_file, name, phone)
-        if add["success"]:
-            print("Success")
-        else:
-            print(add["error"])
-    elif ch["data"] == "search":
-        srch = search.get_id_by_name(crm_file)
-        if srch["success"]:
-            print("Success")
-        else:
-            print(srch["error"])
-    elif ch["data"] == "remove":
-        remove = customer.remove_person_crm(crm_file)
-        if remove["success"]:
-            print("Success")
-        else:
-            print(remove["error"])
-    elif ch["data"] == "update":
-        update = customer.update_person_crm(crm_file)
-        if update["success"]:
-            print("Success")
-        else:
-            print(update["error"])
-    elif ch["data"] == "view":
-        view = customer.view_crm(crm_file)
-        if view["success"]:
-            print("Success")
-        else:
-            print(view["error"])
-    elif ch["data"] == "exit":
-        sys.exit()
-    else:
-        print("Choice not recognised")
-        break
 
