ee6cc2cb580462a6708ac69a46b68392e97001cd
Emmanuel <emmanuelthengumpillil@gmail.com>
2026-01-16 07:09:56 +0530

    Updated get_next_id()

--- core/storage.py
+++ core/storage.py
@@ -3,7 +3,6 @@ import csv
 
 def read_csv(file):
     crm_list = []
-    file_name = file
     try:
         with open(file,'r',newline='') as reading_file:
             reader = csv.DictReader(reading_file)
@@ -11,14 +10,14 @@ def read_csv(file):
             if header == None or header == "" or header == []:
                 return {"success": False, 
                     "data" : "Header is missing", 
-                    "file_name" : file_name,
+                    "file_name" : file,
                     "error" : "File is empty"}
             for row in reader:
                 crm_list.append(row)
             return {"success": True, 
                 "data": crm_list,
                 "header": header,
-                "file_name" : file_name,
+                "file_name" : file,
                 "error" : None}
     except (FileNotFoundError) as e:
         raise FileNotFoundError(e)
@@ -63,12 +62,11 @@ def rewrite_csv(old_file, new_file, new_row):
 
 
 def duplicate_csv(old_file,new_file):
-    crm = old_file["data"]
     old_header = old_file["header"]
     with open(new_file, "w", newline="") as write_file:
         writer = csv.DictWriter(write_file, fieldnames = old_header)
         writer.writeheader()
-        for i in crm:
+        for i in old_file["data"]:
             writer.writerow(i)
 
 
@@ -78,11 +76,9 @@ def get_next_id(file):
         try:
             if crm != []:
                 max_id = int(crm[-1]["Id"])
-
-                # for i in crm:
-                #     for j in crm[i]["Id"]:
-                #         if max_id < j:
-                #             max_id = j
+                for i in range(len(crm)):
+                    if max_id < int(crm[-i]["Id"]):
+                            max_id = int(crm[-i]["Id"])
                 return {"success": True, 
                     "data": max_id + 1, 
                     "error" : None}
